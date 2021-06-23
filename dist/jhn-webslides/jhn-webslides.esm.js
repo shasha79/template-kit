1840df5082af9b10336dc73fd063bfcdea5506b6
shasha79 <alexander.raginsky@gmail.com>
2021-06-23 12:27:16 +0200

    Playing with font and image sizes

---
+++ dist/jhn-webslides/jhn-webslides.esm.js
@@ -0,0 +1,22 @@
+import {p as e, b as t} from './p-1a8d080c.js';
+(() => {
+  const t = import.meta.url,
+    n = {};
+  return '' !== t && (n.resourcesUrl = new URL('.', t).href), e(n);
+})().then((e) =>
+  t(
+    [
+      [
+        'p-3f65cf4e',
+        [
+          [
+            1,
+            'jhn-webslides',
+            {value: [513], beforeSwipe: [64], afterSwipe: [64], lazyLoadContent: [64], revealContent: [64], hideContent: [64]}
+          ]
+        ]
+      ]
+    ],
+    e
+  )
+);
